eb90de9e9fcba38e4ef5fde6b9c260847e04e873
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-11-14 19:09:58 +1100

    Rename buffers to buffer.

--- omgl/__init__.py
+++ omgl/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from . import gl
 
 # VBO style buffers
-from .buffers import buffer, array_buffer, element_buffer
+from .buffer import buffer, array_buffer, element_buffer
 
 # shaders
 from .shader import shader, fragment_shader, vertex_shader, program
